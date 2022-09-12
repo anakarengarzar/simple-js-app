e9fd719d2be4ce5f777f0bf61336b31fc05c9a58
AK <anakarengarzar@gmail.com>
2022-09-12 03:28:42 +0300

    Created array containing 3 pokemon objects

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let pokemonList = [
+    {
+        name: 'Bulbasur',
+        height: 7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Beedrill',
+        height: 1,
+        type: ['bug', 'poison']
+    },
+    {
+        name: 'Pidgeot',
+        height: 1.5,
+        type: ['flying', 'normal']
+    }];
