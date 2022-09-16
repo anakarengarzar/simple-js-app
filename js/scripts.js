c9968480ed5401ae4a53adef24b57b7e189e64e7
AK <anakarengarzar@gmail.com>
2022-09-16 14:32:21 +0300

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,14 @@ let pokemonRepository = (function(){
             type: ['flying', 'normal']
         }];
 
+    //function that will allow to access the pokemonList outside of the IIEF
     function getAll(){
         return pokemonList;
     };
 
+    //function that will allow to add to the pokemonList    
     function add(newPokemon){
-        if (typeof newPokemon === 'object' && 'name' in newPokemon){
+        if (typeof newPokemon === 'object' && 'name' in newPokemon){ //tried to make it work with Object.keys(newPokemon) === ['name', 'height', 'type'] but couldn't make it work.
             console.log('Your new pokemon had been added to the list.')
             return pokemonList.push(newPokemon);
         } else {
@@ -44,5 +46,3 @@ pokemonRepository.getAll().forEach(function(pokemon){ //updated to work with IIE
         document.write(`- ${pokemon.name} (height: ${pokemon.height}m)<br>`); 
     }
 });
-
-//Object.keys(newPokemon) === ['name', 'height', 'type'
